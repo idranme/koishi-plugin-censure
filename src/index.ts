7fe9c1f0fadb7db1133e764cef4c49e51e90e1a2
idanran <idanran@outlook.com>
2024-01-26 23:36:02 +0800

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export function apply(ctx: Context) {
       if (isEmpty(target)) return '你没有输入目标。'
       if (isEmpty(event)) return '你没有输入事件。'
       if (source.includes('&lt;') && source.includes('&gt;')) return '你没有好好说话。'
+      if (target.includes('。') || event.includes('。')) return '你没有好好说话。'
       return `我方对${event}表示强烈谴责。${event}是${target}的蓄意行为，这种行为侵犯了我方的正当权益。我方要求${target}立即停止${event}，并采取措施纠正错误。我方将继续密切关注此事的进展，并采取一切必要措施，以维护我方的正当权益。`
     })
 }
